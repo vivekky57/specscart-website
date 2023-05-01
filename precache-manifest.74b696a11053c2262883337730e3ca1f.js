259e853cd73ae7c23697f792dc85c35e7ce08546
vivekky <vivekky57@gmail.com>
2023-05-02 03:06:43 +0530

    Updates

---
+++ precache-manifest.74b696a11053c2262883337730e3ca1f.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2ea5a4ed561038f1d74",
+    "url": "/specscart-website/static/css/main.df58559e.chunk.css"
+  },
+  {
+    "revision": "b2ea5a4ed561038f1d74",
+    "url": "/specscart-website/static/js/main.b2ea5a4e.chunk.js"
+  },
+  {
+    "revision": "dcf916b92703013e6b2e",
+    "url": "/specscart-website/static/js/1.dcf916b9.chunk.js"
+  },
+  {
+    "revision": "ed8ad7a0fa07cbcb33d9",
+    "url": "/specscart-website/static/js/2.ed8ad7a0.chunk.js"
+  },
+  {
+    "revision": "ff91cd4432fb7f666771",
+    "url": "/specscart-website/static/js/runtime~main.ff91cd44.js"
+  },
+  {
+    "revision": "d4b68090d68f855e4f480d899d92b1a5",
+    "url": "/specscart-website/static/media/Image 39.d4b68090.png"
+  },
+  {
+    "revision": "ca1544ea195ad2b9f4a3acdbd0d740f2",
+    "url": "/specscart-website/static/media/Image 35.ca1544ea.png"
+  },
+  {
+    "revision": "3862105042398b31dce2c6c45fbdb709",
+    "url": "/specscart-website/static/media/Group 6248.38621050.png"
+  },
+  {
+    "revision": "977b9fe92a6f66a102cefe6f51176df5",
+    "url": "/specscart-website/static/media/7.977b9fe9.jpg"
+  },
+  {
+    "revision": "cfb6d3788a6baccf30f6c05c0a877844",
+    "url": "/specscart-website/static/media/Group 6249.cfb6d378.png"
+  },
+  {
+    "revision": "5da49964eeede8c8bd996da5b82fea11",
+    "url": "/specscart-website/static/media/Image 30.5da49964.png"
+  },
+  {
+    "revision": "c65f28d2dc343766d4296e12a523fdf1",
+    "url": "/specscart-website/static/media/anti-glare-lenses-example-active-glare.c65f28d2.jpg"
+  },
+  {
+    "revision": "e07aca3c93b92cf90b86fe2cbd8c18bb",
+    "url": "/specscart-website/static/media/anti-glare-lenses-example-normal.e07aca3c.jpg"
+  },
+  {
+    "revision": "40079039fbe901f2e2065520797808dd",
+    "url": "/specscart-website/static/media/5cb30b93b42e95ee807cc073b66ea1a2.40079039.png"
+  },
+  {
+    "revision": "78662896a21581d28d3f4be68b9f8fb7",
+    "url": "/specscart-website/static/media/808675.78662896.png"
+  },
+  {
+    "revision": "507d86e0f334f36d5818475f990ce312",
+    "url": "/specscart-website/static/media/freepressjournal_import_2017_10_Tom-Cruise.507d86e0.png"
+  },
+  {
+    "revision": "cdb7fca314d75107c4500821c0da8989",
+    "url": "/specscart-website/static/media/Desk4.cdb7fca3.jpg"
+  },
+  {
+    "revision": "d0f40ae99a9527f79d3b0591efaa8196",
+    "url": "/specscart-website/static/media/G1.d0f40ae9.jpg"
+  },
+  {
+    "revision": "fa2d2b2caf8aafe2727a5cb4931eaf42",
+    "url": "/specscart-website/static/media/specscart_awards_follower_rewards.fa2d2b2c.png"
+  },
+  {
+    "revision": "a4c3d7f776384831c767a0155768bf63",
+    "url": "/specscart-website/static/media/Our_feed_img1.a4c3d7f7.png"
+  },
+  {
+    "revision": "191913998a8d3c87f126415854250f11",
+    "url": "/specscart-website/static/media/Our_feed_img2.19191399.png"
+  },
+  {
+    "revision": "5992046698078e5ade4192a9172925fb",
+    "url": "/specscart-website/static/media/Our_feed_img3.59920466.png"
+  },
+  {
+    "revision": "993851b7541c68ebdf120d5b745c495d",
+    "url": "/specscart-website/static/media/Our_feed_img4.993851b7.png"
+  },
+  {
+    "revision": "455894cf504949a536362d3a627950d1",
+    "url": "/specscart-website/index.html"
+  }
+];
